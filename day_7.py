7d84174d2dec4d4d40f44132568b042c606ba22a
d050150 <tibor.tarnai@sap.com>
2023-12-07 11:28:14 +0100

    day 7 #2

--- day_7.py
+++ day_7.py
@@ -2,9 +2,10 @@ from functools import cmp_to_key
 
 card_values = {"A": 13, "K": 12, "Q": 11, "J": 10, "T": 9, "9": 8, "8": 7, "7": 6, "6": 5, "5": 4, "4": 3, "3": 2,
                "2": 1}
-new_card_values = {"A": 13, "K": 12, "Q": 11, "10": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2,
+new_card_values = {"A": 13, "K": 12, "Q": 11, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2,
                    "J": 1}
 kinds = {"5K": [], "4K": [], "FH": [], "3K": [], "2P": [], "1P": [], "HK": []}
+new_kinds = {"5K": [], "4K": [], "FH": [], "3K": [], "2P": [], "1P": [], "HK": []}
 value_map = {}
 
 
@@ -64,21 +65,59 @@ def get_kind(hand: str) -> str:
         return "HK"
 
 
+def get_new_kind(old_kind: str, hand: str) -> str:
+    jokers = 0
+    for i in range(len(hand)):
+        if hand[i] == "J":
+            jokers += 1
+
+    if jokers == 0:
+        return old_kind
+    if old_kind == "5K" or old_kind == "4K":
+        return "5K"
+    if old_kind == "FH":
+        return "5K"
+    if old_kind == "3K":
+        return "4K"
+    if old_kind == "2P":
+        if jokers == 1:
+            return "FH"
+        else:
+            return "4K"
+    if old_kind == "1P":
+        return "3K"
+    if old_kind == "HK":
+        return "1P"
+
+
 with open('day_7.txt') as ifile:
     for line in ifile:
         parts = line.split(" ")
         hand = parts[0].strip()
         value = int(parts[1].strip())
         value_map[hand] = value
-        kind = get_kind(hand)
-        kinds[kind].append(hand)
+        kinds[get_kind(hand)].append(hand)
 
 for kind, hands in kinds.items():
     kinds[kind] = sorted(hands, key=cmp_to_key(compare))
+    for hand in hands:
+        new_kinds[get_new_kind(kind, hand)].append(hand)
+
+for kind, hands in new_kinds.items():
+    new_kinds[kind] = sorted(hands, key=cmp_to_key(compare2))
 
 rank = len(value_map)
 winnings = 0
 for hands in kinds.values():
+    for hand in hands:
+        winnings += rank * value_map[hand]
+        rank -= 1
+
+print(winnings)
+
+rank = len(value_map)
+winnings = 0
+for hands in new_kinds.values():
     for hand in hands:
         winnings += rank * value_map[hand]
         rank -= 1
