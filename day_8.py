1b746af3d469d7265e85ee8cad882d43a195376c
d050150 <tibor.tarnai@sap.com>
2023-12-08 14:52:23 +0100

    day 8 #2

--- day_8.py
+++ day_8.py
@@ -1,6 +1,8 @@
+from math import lcm
+
 instructions = None
 nodes = {}
-
+starts, ends = [], {}
 with open('day_8.txt') as ifile:
     for line in ifile:
         if instructions is None:
@@ -8,6 +10,8 @@ with open('day_8.txt') as ifile:
         elif line.strip() != "":
             parts = line.strip().split("=")
             key = parts[0].strip()
+            if key[2] == "A":
+                starts.append(key)
             node = parts[1].strip()[1:len(parts[1].strip()) - 1].split(",")
             nodes[key] = (node[0].strip(), node[1].strip())
 
@@ -19,7 +23,7 @@ while not found:
     for index in range(len(instructions)):
         if instructions[index] == "L":
             next_node = nodes[next_node][0]
-        elif instructions[index] == "R":
+        else:
             next_node = nodes[next_node][1]
         steps += 1
         if next_node == "ZZZ":
@@ -27,3 +31,34 @@ while not found:
             break
 
 print(steps)
+
+steps = 0
+found = False
+next_nodes = starts
+multipliers = []
+
+while not found:
+    for index in range(len(instructions)):
+        new_next_nodes = []
+        all_end_nodes = True
+        for node in next_nodes:
+            if instructions[index] == "L":
+                next_node = nodes[node][0]
+            else:
+                next_node = nodes[node][1]
+            new_next_nodes.append(next_node)
+            if next_node[2] == "Z":
+                if next_node not in ends:
+                    multipliers.append(steps + 1)
+                ends[next_node] = steps + 1
+
+            if all_end_nodes and next_node[2] != "Z":
+                all_end_nodes = False
+        next_nodes = new_next_nodes
+        steps += 1
+        if len(ends) == len(starts):
+            print(ends)
+            found = True
+            break
+
+print(lcm(*multipliers))
