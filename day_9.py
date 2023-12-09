779dc5654c8f136e9e7974f001b430234d91d92b
Tibor Tarnai <tibor.tarnai@sap.com>
2023-12-09 13:42:28 +0100

    refactor day 9

--- day_9.py
+++ day_9.py
@@ -1,7 +1,5 @@
 from typing import List
 
-sequences = []
-
 
 def generate_next_sequence(sequence: List[int]) -> (List[int], bool):
     new_sequence = []
@@ -14,6 +12,7 @@ def generate_next_sequence(sequence: List[int]) -> (List[int], bool):
     return new_sequence, all_zeroes
 
 
+sequences = []
 with open('day_9.txt') as ifile:
     for line in ifile:
         sequence = []
@@ -21,6 +20,7 @@ with open('day_9.txt') as ifile:
             if n != "":
                 sequence.append(int(n))
         sequences.append(sequence)
+
 result, previous_sum = 0, 0
 for sequence in sequences:
     all_zeroes = False
@@ -30,8 +30,8 @@ for sequence in sequences:
         result += sequence[- 1]
         sequence, all_zeroes = generate_next_sequence(sequence)
     previous = 0
-    for i in range(len(extrapolated_values)):
-        previous = extrapolated_values[-(i + 1)][0] - previous
+    for i in range(len(extrapolated_values) - 1, -1, -1):
+        previous = extrapolated_values[i][0] - previous
     previous_sum += previous
 
 print(result)
