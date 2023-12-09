ef4ec663ae243fc6e01935a1b4cd7884cd767c04
Tibor Tarnai <tibor.tarnai@sap.com>
2023-12-09 11:05:14 +0100

    day 9 #1

---
+++ day_9.py
@@ -0,0 +1,29 @@
+from typing import List
+
+sequences = []
+
+
+def generate_next_sequence(sequence: List[int]) -> (List[int], bool):
+    new_sequence = []
+    all_zeroes = True
+    for i in range(len(sequence) - 1):
+        diff = sequence[i + 1] - sequence[i]
+        new_sequence.append(diff)
+        if all_zeroes and diff != 0:
+            all_zeroes = False
+    return new_sequence, all_zeroes
+
+
+with open('day_9.txt') as ifile:
+    for line in ifile:
+        sequence = []
+        for n in line.strip().split(" "):
+            if n != "":
+                sequence.append(int(n))
+        sequences.append(sequence)
+
+for s in sequences:
+    all_zeroes = False
+    while not all_zeroes:
+        s, all_zeroes = generate_next_sequence(s)
+        print(s)
