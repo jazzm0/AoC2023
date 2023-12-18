1fdb85f051294811e77f0129460557cba899aedd
d050150 <tibor.tarnai@sap.com>
2023-12-18 18:22:48 +0100

    day 18 #1

---
+++ day_18.py
@@ -0,0 +1,49 @@
+from typing import List
+
+from PIL import Image, ImageDraw
+
+directions = {"L": (0, -1), "R": (0, 1), "U": (-1, 0), "D": (1, 0)}
+
+
+def parse_instructions(f: str) -> List[tuple]:
+    instructions = []
+    with open(f) as ifile:
+        for line in ifile:
+            parts = line.strip().split()
+            direction = directions[parts[0]]
+            instructions.append((direction, int(parts[1])))
+    return instructions
+
+
+def dig(instructions: List[tuple]):
+    position = (0, 0)
+    plan = [position]
+    min_x, max_x, min_y, max_y = 0, 0, 0, 0
+    for instruction in instructions:
+        direction = instruction[0]
+        steps = instruction[1]
+        for i in range(steps):
+            position = (position[0] + direction[0], position[1] + direction[1])
+            min_x = min(min_x, position[0])
+            max_x = max(max_x, position[0])
+            min_y = min(min_y, position[1])
+            max_y = max(max_y, position[1])
+            plan.append(position)
+    m, n = max_x - min_x + 1, max_y - min_y + 1
+
+    im = Image.new(mode="RGB", size=(m, n), color=(0, 0, 0))
+    for i in range(len(plan)):
+        im.putpixel(xy=(plan[i][0] + abs(min_x), plan[i][1] + abs(min_y)), value=(255, 255, 255))
+
+    ImageDraw.floodfill(im, (150, 150), value=(255, 255, 255))
+    im.save("map.png")
+    dug_out = 0
+    for pixel in im.getdata():
+        if pixel == (255, 255, 255):
+            dug_out += 1
+
+    return dug_out
+
+
+instructions = parse_instructions("day_18.txt")
+print(dig(instructions))
