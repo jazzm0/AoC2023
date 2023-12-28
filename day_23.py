1bba89a96f8af4a04ad268420922fe5749f9e2de
d050150 <tibor.tarnai@sap.com>
2023-12-28 15:37:28 +0100

    day 23 refactor

--- day_23.py
+++ day_23.py
@@ -41,27 +41,57 @@ def get_next_positions(map: List[str], actual_position: tuple, seen: dict) -> Li
     return next_positions
 
 
-def get_longest_path(map: List[str]) -> int:
+def get_next_positions_new(map: List[str], actual_position: tuple, visited: dict) -> List[tuple]:
+    m, n = len(map), len(map[0])
+    next_positions = []
+    if actual_position[0] > 0:
+        up = (actual_position[0] - 1, actual_position[1])
+        if up not in visited and map[up[0]][up[1]] != '#':
+            next_positions.append(up)
+
+    if actual_position[0] < m - 1:
+        down = (actual_position[0] + 1, actual_position[1])
+        if down not in visited and map[down[0]][down[1]] != '#':
+            next_positions.append(down)
+
+    if actual_position[1] > 0:
+        left = (actual_position[0], actual_position[1] - 1)
+        if left not in visited and map[left[0]][left[1]] != '#':
+            next_positions.append(left)
+
+    if actual_position[1] < n - 1:
+        right = (actual_position[0], actual_position[1] + 1)
+        if right not in visited and map[right[0]][right[1]] != '#':
+            next_positions.append(right)
+
+    return next_positions
+
+
+def build_graph(map: List[str], np) -> (tuple, dict):
     start = None
-    seen = {}
+    graph = {}
     for i in range(len(map[0])):
         if map[0][i] == ".":
             start = (0, i)
             break
 
-    next_positions = get_next_positions(map, start, seen)
-    seen[start] = next_positions
+    next_positions = np(map, start, graph)
+    graph[start] = next_positions
     while len(next_positions) != 0:
         new_next_positions = set()
         for position in next_positions:
-            if position in seen:
+            if position in graph:
                 continue
-            next_positions_from_actual = get_next_positions(map, position, seen)
-            seen[position] = next_positions_from_actual
+            next_positions_from_actual = np(map, position, graph)
+            graph[position] = next_positions_from_actual
             for p in next_positions_from_actual:
                 new_next_positions.add(p)
         next_positions = new_next_positions
+    return start, graph
 
+
+def get_longest_path(map: List[str]) -> int:
+    start, graph = build_graph(map, get_next_positions)
     longest = 0
     finished = False
     while not finished:
@@ -70,7 +100,7 @@ def get_longest_path(map: List[str]) -> int:
         finished = True
         last = None
         steps = 0
-        next_position = seen[actual]
+        next_position = graph[actual]
         while len(next_position) != 0:
             steps += 1
             if len(next_position) > 1:
@@ -78,10 +108,10 @@ def get_longest_path(map: List[str]) -> int:
                 last = actual
             actual = next_position[0]
             path.append(actual)
-            next_position = seen[actual]
+            next_position = graph[actual]
         longest = max(longest, steps)
         if last is not None:
-            del seen[last][0]
+            del graph[last][0]
     return longest
 
 
